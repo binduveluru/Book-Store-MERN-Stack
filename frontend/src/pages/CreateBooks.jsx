0625702fb657f60beee3a0e7622427e9227b8ed8
binduveluru <bindu@rekonsys.tech>
2024-04-16 15:51:31 +0530

    added backend url

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
-import BackButton from '../components/BackButton';
-import Spinner from '../components/Spinner';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { useSnackbar } from 'notistack';
+import React, { useState } from "react";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useSnackbar } from "notistack";
 
 const CreateBooks = () => {
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
-  const [publishYear, setPublishYear] = useState('');
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [publishYear, setPublishYear] = useState("");
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const { enqueueSnackbar } = useSnackbar();
@@ -21,65 +21,65 @@ const CreateBooks = () => {
     };
     setLoading(true);
     await axios
-      .post('http://localhost:3005/books', data)
+      .post("https://book-store-mern-stack-bk85.onrender.com/books", data)
       .then(() => {
         setLoading(false);
         // enqeuesnackbar it showing a small notification mes // snackbar is a just small notifation mes showing
 
-        enqueueSnackbar('Book Created successfully', { variant: 'success' });
-        // this "navigate" helps back to the home page 
+        enqueueSnackbar("Book Created successfully", { variant: "success" });
+        // this "navigate" helps back to the home page
 
-        navigate('/');
+        navigate("/");
       })
       .catch((error) => {
         setLoading(false);
-        // alert('An error happened. Please Chack console'); when thier is an error it's showing a error mes 
-        
-        enqueueSnackbar('Error', { variant: 'error' });
+        // alert('An error happened. Please Chack console'); when thier is an error it's showing a error mes
+
+        enqueueSnackbar("Error", { variant: "error" });
 
         console.log(error);
       });
   };
 
   return (
-    <div className='p-4'>
+    <div className="p-4">
       <BackButton />
-      <h1 className='text-3xl my-4'>Create Book</h1>
-      {loading ? <Spinner /> : ''}
-      <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
-        <div className='my-4'>
-          <label className='text-xl mr-4 text-gray-500'>Title</label>
+      <h1 className="text-3xl my-4">Create Book</h1>
+      {loading ? <Spinner /> : ""}
+      <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500">Title</label>
           <input
-            type='text'
+            type="text"
             value={title}
             onChange={(e) => setTitle(e.target.value)}
-            className='border-2 border-gray-500 px-4 py-2 w-full'
+            className="border-2 border-gray-500 px-4 py-2 w-full"
           />
         </div>
-        <div className='my-4'>
-          <label className='text-xl mr-4 text-gray-500'>Author</label>
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500">Author</label>
           <input
-            type='text'
+            type="text"
             value={author}
             onChange={(e) => setAuthor(e.target.value)}
-            className='border-2 border-gray-500 px-4 py-2  w-full '
+            className="border-2 border-gray-500 px-4 py-2  w-full "
           />
         </div>
-        <div className='my-4'>
-          <label className='text-xl mr-4 text-gray-500'>Publish Year</label>
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500">Publish Year</label>
           <input
-            type='number'
+            type="number"
             value={publishYear}
             onChange={(e) => setPublishYear(e.target.value)}
-            className='border-2 border-gray-500 px-4 py-2  w-full '
+            className="border-2 border-gray-500 px-4 py-2  w-full "
           />
         </div>
-        <button className='p-2 bg-sky-300 m-8' onClick={handleSaveBook}>
+        <button className="p-2 bg-sky-300 m-8" onClick={handleSaveBook}>
           Save
         </button>
       </div>
     </div>
   );
-}
+};
 
-export default CreateBooks
+export default CreateBooks;
