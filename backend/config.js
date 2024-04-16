b0468750d1660e1dc4b8d15a151ddbe9221c0c65
binduveluru <bindu@rekonsys.tech>
2024-04-16 15:07:10 +0530

    added mongodb url

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,4 @@
 export const PORT = 3005;
 
 export const mongoDBURL =
-  'mongodb://localhost:27017/Books';
-
+  "mongodb+srv://bindu:Cs5oxZlW69w3Wa3c@cluster0.k8jxjah.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
